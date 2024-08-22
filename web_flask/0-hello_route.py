107ee5f9ad08d17acd38440edca718e39c2a2a40
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-08-22 19:01:11 +0300

    test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-	return 'Hello HBNB!'
+def start_flask():
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
